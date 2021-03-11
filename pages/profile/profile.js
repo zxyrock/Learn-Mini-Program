127c84b1271f6d8a616d5f0e85183ea524335c99
rock <1468943810@qq.com>
2021-03-11 21:41:33 +0800

    初体验

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,34 +1,11 @@
-// pages/home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-       name:'zxyrock',
-       age:23,
-       team: [
-        {id: 111,name:'唐三藏',age: 20},
-        {id: 112,name:'孙悟空',age: 600},
-        {id: 113,name:'猪八戒',age: 800},
-        {id: 114,name:'沙和尚',age: 1000},
-        {id: 115,name:'白龙马',age: 700}
-       ],
-       counter:0
-  },
-  handleBtnClick(){
-    // 这种做法是错误的做法，界面是不会刷新的，不同于vue的双向绑定
-    // this.data.counter += 1
 
-    // 2.方法:this.setData()来拿到data中的数据进行操作
-    this.setData({
-      counter: this.data.counter + 1
-    })
-  },
-  handleSubtraction(){
-    this.setData({
-      counter: this.data.counter - 1
-    })
   },
 
   /**
