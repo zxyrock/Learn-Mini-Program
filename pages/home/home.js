d595be110cbbf1c48a8a2b24d03daafb9103ba70
rock <1468943810@qq.com>
2021-03-17 21:06:29 +0800

    04_注册Page时做什么

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,10 @@
 const  app = getApp()
 console.log(app.globalData.name)
 console.log(app.globalData.age)
+const name = app.globalData.name;
+const age = app.globalData.age;
 
+// Page()方法注册页面
 Page({
 
   /**
@@ -20,8 +23,10 @@ Page({
         {id: 114,name:'沙和尚',age: 1000},
         {id: 115,name:'白龙马',age: 700}
        ],
-       counter:0
+       counter:0,
+       code:0
   },
+  // 可以自定义一些事件
   handleBtnClick(){
     // 这种做法是错误的做法，界面是不会刷新的，不同于vue的双向绑定
     // this.data.counter += 1
@@ -39,18 +44,32 @@ Page({
   handleGetUserInfo(event){
     console.log(event)
   },
+  handlaViewClick(){
+    console.log('code被点击了')
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+     console.log('onLoad--页面加载')
+     wx.request({
+       url: 'https://api.apiopen.top/getJoke?page=1&count=2&type=video',
+       success:(res) => {
+         console.log(res)
+         const code = res.data.code;
+         this.setData({
+           code : code
+         })
+       }
+     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log('onReady--页面初次渲染完成')
 
   },
 
@@ -58,6 +77,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log('onShow--页面显示')
 
   },
 
@@ -65,6 +85,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+    console.log('onHide--页面隐藏')
 
   },
 
@@ -72,6 +93,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+    console.log('onUnload--页面卸载即发生跳转')
 
   },
 
@@ -79,6 +101,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    console.log('onPullDownRefresh--用户下拉了，配套刷新')
 
   },
 
@@ -86,6 +109,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    console.log('onReachBottom--用户上拉，滚动到底部')
 
   },
 
@@ -93,6 +117,12 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    console.log('onShareAppMessage--分享出去')
 
+  },
+  // 其他事件
+  // 监听页面滚动事件
+  onPageScroll(obj){
+    console.log(obj)
   }
 })
