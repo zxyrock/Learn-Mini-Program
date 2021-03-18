10059bc3609ba1d68cb85b716f61b02c2d448317
rock <1468943810@qq.com>
2021-03-19 00:05:48 +0800

    08_input组件的使用

---
+++ pages/input/input.js
@@ -0,0 +1,19 @@
+// pages/input/input.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  handleInput(event){
+    console.log('用户输入内容',event)
+  },
+  hanleFocus(event){
+    console.log('input获取焦点',event)
+  },
+  handleBlur(event){
+    console.log('input失去焦点',event)
+  }
+})
