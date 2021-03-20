46867421ca486c2bd674bccbe7cd7ddc4868c7b1
rock <1468943810@qq.com>
2021-03-20 23:50:12 +0800

    15_wxml语法-列表渲染

---
+++ pages/wxml/wxml.js
@@ -0,0 +1,73 @@
+// pages/wxml/wxml.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     team:['唐僧','孙悟空','猪八戒','沙和尚','白龙马'],
+     nums:[
+       [1,3,5,7,9],
+       [2,4,6,8,0],
+       ['a','b','c','d','e'],
+       ['唐僧','孙悟空','猪八戒','沙和尚','白龙马']
+     ],
+     letters:[1,2,3,1]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
