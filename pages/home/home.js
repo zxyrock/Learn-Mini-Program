5c179864ae6f394b126f26e4791b5698c9a1690d
rock <1468943810@qq.com>
2021-03-23 00:03:33 +0800

    21_事件的冒泡与捕获

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,23 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 事件的冒泡和捕获
+  handleCaptureView1() {
+    console.log("监听到最外层view1的捕获事件")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleBindView1() {
+    console.log("监听到最外层view1的冒泡事件")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleCaptureView2() {
+    console.log("监听到中层view2的捕获事件")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleBindView2() {
+    console.log("监听到中层view2的冒泡事件")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleCaptureView3() {
+    console.log("监听到最内层view3的捕获事件")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBindView3() {
+    console.log("监听到最内层view3的冒泡事件")
   }
 })
