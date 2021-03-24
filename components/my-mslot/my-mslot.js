ec4d806c3dcaea8c6503685d95934a40a25d2c33
rock <1468943810@qq.com>
2021-03-24 23:42:05 +0800

    29_插槽slot的使用

---
+++ components/my-mslot/my-mslot.js
@@ -0,0 +1,27 @@
+// components/my-mslot/my-mslot.js
+Component({
+  options:{
+    // 只有将multipleSlots属性设置为true，才能使用多插槽
+    multipleSlots:true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
