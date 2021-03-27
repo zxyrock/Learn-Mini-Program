2a99f9ae658d6b7fc07ba5cc59a70e22453da770
rock <1468943810@qq.com>
2021-03-27 23:51:00 +0800

    32_网络请求封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
 
   /**
@@ -12,18 +13,51 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 发送网络请求
-    wx.request({
-      url: 'https://api.apiopen.top/getJoke?',
+    // -----1.发送网络请求(原生方式，太古老)------
+    this.get_data_origin()
+    // -----2.使用封装的request发送网络请求
+    request({
+      url:"https://api.apiopen.top/getJoke?",
       data:{
         page:1,
         count:2,
         type:'video'
-      },
-      success:function(res){
-        console.log(res)
       }
+    }).then(res =>{
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
     })
+
+   
+  },
+  get_data_origin(){
+    // wx.request({
+    //   // 第一种写法：直接将网址写上
+    //   // url:'https://api.apiopen.top/getJoke?page=1&count=2&type=video',
+    //   // 第二种写法:将网址拆分，用data拼接
+    //   url: 'https://api.apiopen.top/getJoke?',
+    //   // method:用于设置请求方式，默认为get请求
+    //   // method:'POST'
+    //   data:{
+    //     page:1,
+    //     count:2,
+    //     type:'video'
+    //   },
+    //   // success:成功的回调
+    //   success:function(res){
+    //     console.log(res)
+    //   },
+    //   // fail:失败的回调
+    //   fail:function(err){
+    //     console.log(err)
+    //   },
+    //   // complete:接口调用完后会回调，不论成功还是调用失败
+    //   complete:function(){
+    //     console.log('---')
+    //   }
+    // })
+    
   },
 
   /**
