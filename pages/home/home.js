23de4cc09443290e1c368037b6be8d4b4cac3f5f
rock <1468943810@qq.com>
2021-03-27 22:32:19 +0800

    31_网络请求的基本使用

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,37 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-    counter:0,
-    isShow:true
+    
   },
-  // 组件传递过来的数据对象在event中的detail中
-  handleIncrement(event){
-    console.log('-----',event)
-    this.setData({
-      counter:this.data.counter + 1
-    })
-  },
-  tabControlClick(event){
-    console.log(event)
-  },
-  handleIncrementCpn(){
-    // console.log("--------")
-    // 最终目的：修改my-sel组件中的counter数据
-    // 1.先获组件对象的方法：this.selectComponent("组件对象绑定的class或id")
-    const my_sel = this.selectComponent('.sel-class')
-    // console.log(my_sel)
-    // 2.通过setData()来修改组件中的数据，但这种方式不合理，因为这是直接修改组件内的数据，
-    // 若是其他页面也用该组件的此数据，会一起造成影响
-    // my_sel.setData({
-    //   counter: my_sel.data.counter + 10
-    // })
 
-    // 3.通过调用组件自己的方法对数据进行修改
-    my_sel.incrementCounter(10)
-  },
-  handleChangeShow(){
-    this.setData({
-      isShow:!this.data.isShow
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 发送网络请求
+    wx.request({
+      url: 'https://api.apiopen.top/getJoke?',
+      data:{
+        page:1,
+        count:2,
+        type:'video'
+      },
+      success:function(res){
+        console.log(res)
+      }
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
