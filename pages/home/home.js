93e26586430fd5f4971a8876a8bbb1308cbe53f1
rock <1468943810@qq.com>
2021-04-07 23:01:47 +0800

    33_展示弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,6 +8,74 @@ Page({
   data: {
     
   },
+  handleShowToast(){
+    wx.showToast({
+      // title:设置弹窗显示内容,文本最多显示 7 个汉字长度
+      title: '加载中...',
+      // duration:设置弹窗显示时间
+      duration:3000,
+      // icon:设置显示的图标，微信提供了一些图标
+      icon:'loading',
+      //mask:	是否显示透明蒙层，防止触摸穿透,设置为true则在显示弹窗时，其他不能进行点击
+      mask:true,
+      // success：接口调用成功的回调函数
+      success:function(){
+        console.log('弹窗展示成功')
+      },
+      // fail：接口调用失败的回调函数
+      fail:function(){
+        console.log('弹窗展示失败')
+      },
+      // complete：接口调用结束的回调函数（调用成功、失败都会执行）
+      complete:function(){
+        console.log('不管成功还是失败都会调用complete函数，即接口调用完成')
+      }
+    })
+  },
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content:'我是内容：Hello World',
+      // showCancel:是否显示取消按钮
+      showCancel:true,
+      // cancelText：设置取消按钮的文字，最多 4 个字符
+      cancelText:'退出',
+      // cancelColor:取消按钮的文字颜色
+      cancelColor:'red',
+      // success：接口调用成功的回调函数
+      success:function(res){
+        console.log(res)
+        if(res.confirm){
+          console.log('用户点击了确定按钮')
+        }
+        if(res.concel){
+          console.log('用户点击了取消按钮')
+        }
+      }
+    })
+  },
+  handleShowLoading(){
+    // wx.showLoading不会主动消失
+    wx.showLoading({
+      title: '加载ing',
+      mask:true
+    })
+    // 必须手动调用 wx.hideLoading函数才会让wx.showLoading的弹窗消失
+    setTimeout(()=>{
+      wx.hideLoading({
+        success: (res) => {},
+      })
+    },1000)
+  },
+  handleActionSheet(){
+   wx.showActionSheet({
+     itemList: ['相册','拍照'],
+     itemColor:'red',
+     success:function(res){
+       console.log(res)
+     }
+   })
+  },
 
   /**
    * 生命周期函数--监听页面加载
