591c545aafae2571458ab9abf086646bb4fce558
rock <1468943810@qq.com>
2021-04-19 22:46:29 +0800

    42_首页商品数据的请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,7 @@
 // pages/home/home.js
 import {
-  getMultiData
+  getMultiData,
+  getGoogsData
 }from "../../service/home.js"
 
 Page({
@@ -11,7 +12,12 @@ Page({
   data: {
     banners:[],
     recommends:[],
-    titles:['流行','新款','精选']
+    titles:['流行','新款','精选'],
+    goods:{
+      'pop':{page:0,list:[]},
+      'new':{page:0,list:[]},
+      'sell':{page:0,list:[]}
+    }
 
   },
 
@@ -20,26 +26,67 @@ Page({
    */
   onLoad: function (options) {
     // 1.页面加载完，立刻发送网络请求，请求数据
-    // 1.1 请求轮播图数据以及推荐数据
-    getMultiData().then(res => {
-      // 1.2 取出轮播图和推荐的数据
-      const banners = res.data.data.banner.list;
-      const recommends = res.data.data.recommend.list;
-      // 1.3 将请求到的banners和recommends放到data中
-      this.setData({
-        banners,
-        recommends
-      })
+    this._getMultidata()
 
-    }).catch(err => {
-      wx.showToast({
-        title: '加载出错',
-        icon:"loading",
-        duration:3000
-      })
-    }) 
+    // 2.请求商品数据
+    this._getGoodsData('pop')
+    this._getGoodsData('new')
+    this._getGoodsData('sell')
+
 
   },
+
+  // -------------网络请求函数---------------
+  // 1.自定义请求轮播图函数
+  _getMultidata(){
+        // 1.1 请求轮播图数据以及推荐数据
+        getMultiData().then(res => {
+          // 1.2 取出轮播图和推荐的数据
+          const banners = res.data.data.banner.list;
+          const recommends = res.data.data.recommend.list;
+          // 1.3 将请求到的banners和recommends放到data中
+          this.setData({
+            banners,
+            recommends
+          })
+    
+        }).catch(err => {
+          wx.showToast({
+            title: '加载出错',
+            icon:"loading",
+            duration:3000
+          })
+        }) 
+  },
+  // 2.自定义请求商品数据函数
+  _getGoodsData(type){
+    // 2.1.获取页码
+    const page = this.data.goods[type].page + 1
+
+    //2.2.发送网络请求
+    getGoogsData(type,page).then(res => {
+      // console.log(res)
+      // 2.2.1.取出数据list
+      const list = res.data.data.list;
+
+      // 2.2.2.将数据设置到对应type的list里（...语法）
+      const oldList = this.data.goods[type].list;
+      oldList.push(...list)
+
+      // 2.2.3. 将数据设置到data中的goods中
+      const typeKey = `goods.${type}.list`;
+      const pageKey = `goods.${type}.page`;
+      this.setData({
+        [typeKey]:oldList,
+        [pageKey]:page
+      })
+    })
+
+
+  },
+
+
+  // -----------------事件监听函数-----------------
   tabControlClick(event){
      // 取出index
      const index = event.detail.index;
--- service/home.js
+++ service/home.js
@@ -1,7 +1,19 @@
 import request from "./network";
 
+// 请求轮播图
 export function getMultiData(){
   return request({
     url:"/home/multidata"
+  })
+}
+
+// 请求商品数据
+export function getGoogsData(type,page){
+  return request({
+    url:"/home/data",
+    data:{
+      type,
+      page
+    }
   })
 }
